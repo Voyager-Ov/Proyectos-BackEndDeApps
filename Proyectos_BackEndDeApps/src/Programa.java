025185ccaeb869ee48de5d405470f8b49204bb00
Voyager-Ov <octavio.velo2022@gmail.com>
2024-08-20 13:26:21 -0300

    agrego unas variables

--- Proyectos_BackEndDeApps/src/Programa.java
+++ Proyectos_BackEndDeApps/src/Programa.java
@@ -1,5 +1,12 @@
 public class Programa {
+/*
+*para crear una clase se necesita que esta se llame igual que el archivo.java
+* public class se refiere que todos pueden usar esta clase
+*/
     public static void main(String[] args) {
+        // siempre que se quiera poner variable se necesita aclarar de qué tipo es.
         System.out.println("Hola Mundo");
+        int a = 3;
+        System.out.println(a);
     }
 }
